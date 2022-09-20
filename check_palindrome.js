e43269a12f846a183d4c9c40a6f4be0c65a32624
unknown <surajkumar231501@gmail.com>
2022-09-20 10:25:26 +0530

    complete code

--- check_palindrome.js
+++ check_palindrome.js
@@ -1,6 +1,13 @@
 
 
-let str="naman";
+let str = "naman";
 
-
-	console.log(str,"it's palindrome");
+let str1 = "";
+for (let i = str.length - 1; i >= 0; i--) {
+	str1 += str[i];
+}
+if (str1 == str) {
+	console.log("Palindrome");
+} else {
+	console.log("Not a Palindrome");
+}
